a91c5cd9e237d1383f6964bdb2a9a49fbb163fc2
Martin Sandve Alnæs <msa@databutton.io>
2024-06-16 22:35:38 +0200

    try tapping term 120

--- keyboards/massdrop/alt/keymaps/martinal/config.h
+++ keyboards/massdrop/alt/keymaps/martinal/config.h
@@ -1,7 +1,9 @@
 #pragma once
 
 // https://docs.qmk.fm/tap_hold
-#define TAPPING_TERM 90
+// 200 too long
+// 90 too short
+#define TAPPING_TERM 120
 
 // #define UNICODE_SELECTED_MODES UNICODE_MODE_BSD, UNICODE_MODE_MACOS, UNICODE_MODE_LINUX
 
