fb013d297b67a2742eac1d7acb8fe7e374bf32d5
Martin Sandve Alnæs <msa@databutton.io>
2024-06-16 22:41:14 +0200

    tap term 150

--- keyboards/massdrop/alt/keymaps/martinal/config.h
+++ keyboards/massdrop/alt/keymaps/martinal/config.h
@@ -1,9 +1,12 @@
 #pragma once
 
 // https://docs.qmk.fm/tap_hold
-// 200 too long
+// default 200 is too long for caps as esc
 // 90 too short
-#define TAPPING_TERM 120
+// 120 ok for capslock, too short for tab
+// 150 seems to work ok
+// consider finetuning and per-key config
+#define TAPPING_TERM 150
 
 // #define UNICODE_SELECTED_MODES UNICODE_MODE_BSD, UNICODE_MODE_MACOS, UNICODE_MODE_LINUX
 
