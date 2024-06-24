19b779d7aa5623aa8e20735c4a7d9f5fd06b0a41
Martin Sandve Alnæs <msa@databutton.io>
2024-06-24 18:02:32 +0200

    reduce tap term

--- keyboards/massdrop/alt/keymaps/martinal/config.h
+++ keyboards/massdrop/alt/keymaps/martinal/config.h
@@ -2,10 +2,9 @@
 
 // https://docs.qmk.fm/tap_hold
 // default 200 is too long for caps as esc
-// 90 too short
-// 120 ok for capslock, too short for tab
-// 150 seems to work ok
-#define TAPPING_TERM 150
+// Using Fn+x Fn+c Fn+v to reduce, increase, print this value
+// 140 seems ok
+#define TAPPING_TERM 140
 
 // finetuning per-key config, see get_tapping_term in keymap.c
 #define TAPPING_TERM_PER_KEY
